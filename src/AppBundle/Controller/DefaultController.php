6aaa2b4082a55d455d4077aa8e8656ca61533cbc
Benoît Frisch <benoit@fresh.lu>
2017-10-26 03:14:47 +0200

    Accelerate Search by ajax

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Word;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -20,6 +23,34 @@ class DefaultController extends Controller
         return [
             'words' => $words
         ];
+    }
+
+    /**
+     *
+     * @Route("/search/{query}", name="search")
+     * @return Response
+     */
+    public function searchAction($query)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $word = $em
+            ->getRepository(Word::class);
+
+        $queryWord = $word->createQueryBuilder('a')
+            ->where('a.latin_raw LIKE :query')
+            ->orderBy('a.latin', 'ASC')
+            ->setParameter('query', "%" . $query . "%");
+        $words = $queryWord->getQuery()->getResult();
+
+        $arr = [];
+        foreach ($words as $key => $value) {
+            $arr[] = ["id" => $value->getId(), "raw" => $value->getLatinRaw(), "latin" => $value->getLatin()];
+        }
+
+
+        return JsonResponse::fromJsonString(json_encode($arr));
+
     }
 
     /**
